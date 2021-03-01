d416c194e11c705993d54d22fbf3db05236b9916
陈家傲 <chenjiaao@laoyuegou.com>
2021-03-01 14:58:40 +0800

    fix

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use NSQClient\Access\Endpoint;
+use NSQClient\Message\Message;
+use NSQClient\Queue;
 use phpspider\core\phpspider;
 
 class Test extends Command
@@ -38,38 +41,11 @@ class Test extends Command
      */
     public function handle()
     {
-        $configs = array(
-            'name' => '糗事百科',
-            'log_show' => false,
-            'domains' => array(
-                'qiushibaike.com',
-                'www.qiushibaike.com'
-            ),
-            'scan_urls' => array(
-                'http://www.qiushibaike.com/'
-            ),
-            'content_url_regexes' => array(
-                "http://www.qiushibaike.com/article/\d+"
-            ),
-            'list_url_regexes' => array(
-                "http://www.qiushibaike.com/8hr/page/\d+\?s=\d+"
-            ),
-            'fields' => array(
-                array(
-                    // 抽取内容页的文章内容
-                    'name' => "article_content",
-                    'selector' => "//*[@id='single-next-link']",
-                    'required' => true
-                ),
-                array(
-                    // 抽取内容页的文章作者
-                    'name' => "article_author",
-                    'selector' => "//div[contains(@class,'author')]//h2",
-                    'required' => true
-                ),
-            ),
-        );
-        $spider = new phpspider($configs);
-        $spider->start();
+        $topic = 'my_topic';
+        $endpoint = new Endpoint('http://127.0.0.1:4161');
+        $message = new Message('hello world');
+        $result = Queue::publish($endpoint, $topic, $message);
+
+        dd($result);
     }
 }
--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,11 +7,19 @@ use App\Services\Notices\Star;
 use App\Services\Notices\Weather;
 use App\Utils\CurlUtil;
 use Illuminate\Http\Request;
+use NSQClient\Access\Endpoint;
+use NSQClient\Queue;
+use NsqClient\Message\Message;
 
 class TestController extends Controller
 {
     public function tests()
     {
-        dd(route('star.add'));
+        $topic = 'my_topic';
+        $endpoint = new Endpoint('http://127.0.0.1:4161');
+        $message = new Message('hello world');
+        $result = Queue::publish($endpoint, $topic, $message);
+
+        dd($result);
     }
 }
